0ca625dc3d3340e618568a1a27e64bcf9cb220c1
codewithrohit370 <rohitbgmi370@gmail.com>
2026-08-10 00:39:49 +0530

    More Song

--- script/song.js
+++ script/song.js
@@ -22,16 +22,51 @@ export let songs = [
     },
     {
         songId:4,
-        songName:"Maine Pyar Kiya",
+        songName:"Thodi Der",
         ArtistName:"Lata Mangeshkar",
-        songImage:"images/maien-pyar-kiya.jpg",
-        songSrc:"song/maien pyar kiya.mp3" 
+        songImage:"images/Thodi Dar.jpg",
+        songSrc:"song/Thodi Der -Full Video Half Girlfriend Arjun Kapoor Shraddha Kapoor Farhan S Shreya Ghoshal.mp3" 
     },
     {
         songId:5,
-        songName:"Maine Pyar Kiya",
+        songName:"Dance Ka Bhoot",
         ArtistName:"Lata Mangeshkar",
-        songImage:"images/maien-pyar-kiya.jpg",
-        songSrc:"song/maien pyar kiya.mp3", 
-    }
+        songImage:"images/Bramsamaster.jpg",
+        songSrc:"song/Dance Ka Bhoot - Brahmāstra Ranbir Kapoor Alia Bhatt Pritam Arijit Singh Amitabh B.mp3", 
+    },
+    {
+        songId:6,
+        songName:"Bholi Si Surat",
+        ArtistName:"Lata Mangeshkar , Udit Narayan",
+        songImage:"images/bholi si surat.jfif",
+        songSrc:"song/Bholi Si Surat Song Dil To Pagal Hai Shah Rukh Khan, Madhuri Dixit, Karisma Kapoor Lata, Udit.mp3", 
+    },
+    {
+        songId:7,
+        songName:"Aankhein Khuli",
+        ArtistName:"Udit Narayan",
+        songImage:"images/mohabatie.jpg",
+        songSrc:"song/Aankhein Khuli Song Mohabbatein Shah Rukh Khan, Aishwarya Rai Lata Mangeshkar, Udit Narayan.mp3", 
+    },
+    {
+        songId:8,
+        songName:"Tujhe Dekha Toh",
+        ArtistName:"Lata Mangeshkar , Kumar Sanu",
+        songImage:"images/tujhe dakha to yha jana sanam.jfif",
+        songSrc:"song/Tujhe Dekha Toh Song Dilwale Dulhania Le Jayenge Shah Rukh Khan, Kajol Lata, Kumar Sanu DDLJ.mp3", 
+    },
+    {
+        songId:9,
+        songName:"Zara Zara",
+        ArtistName:"Bombay Jayashri",
+        songImage:"images/zara zara.jfif",
+        songSrc:"song/Zara Zara With Lyrics _ ज़रा ज़रा _ Rehna Hai Tere Dil Mein _ R. Madhavan _ Bombay Jayashri _ RHTDM [a71xD6RyOok].mp3", 
+    },
+    {
+        songId:10,
+        songName:"Dil To Pagal Hai",
+        ArtistName:"Lata Mangeshkar , Udit Naryan",
+        songImage:"images/bholi si surat.jfif",
+        songSrc:"song/Dil To Pagal Hai Song Shah Rukh Khan, Madhuri, Karisma, Akshay Lata Mangeshkar, Udit Narayan.mp3", 
+    },
 ]
